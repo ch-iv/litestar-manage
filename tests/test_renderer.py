983744812e8688a7be171407aae4a7f91d269754
Agustin Arce <agustinarceprincich@gmail.com>
2024-09-13 15:52:04 -0400

    refactor (test_renderer): fix typo

--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -4,7 +4,7 @@ from testfixtures import TempDirectory
 
 from litestar_manage.cli import project
 from litestar_manage.renderer import AppRenderingContext, _render_jinja_dir
-from tests import TEMPLATE_DIR
+from tests import APP_TEMPLATE_DIR
 
 
 @pytest.fixture
@@ -20,7 +20,7 @@ def runner() -> CliRunner:
 def test_render_jinja_dir(rendering_context: AppRenderingContext, runner: CliRunner) -> None:
     with TempDirectory() as t:
         temp_path = t.as_path()
-        _render_jinja_dir(TEMPLATE_DIR, temp_path, rendering_context)
+        _render_jinja_dir(APP_TEMPLATE_DIR, temp_path, rendering_context)
 
         assert (temp_path / "src").exists()
         assert (temp_path / "tests").exists()
